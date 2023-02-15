778b1ab85f89e4bb233a0ab50520cb30d4d4e483
Yamin Raad <ashabulraad@gmail.com>
2023-02-16 02:03:38 +0600

    Feature

--- src/Components/Home/Home/Home.jsx
+++ src/Components/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Feature from '../Feature/Feature';
 import Welcome from '../Welcome/Welcome';
 
 const Home = () => {
     return (
         <div>
             <Welcome></Welcome>
+            <Feature></Feature>
         </div>
     );
 };
